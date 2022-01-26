baea51295fbd3188a2f2cc8218c171f00ee749cb
kittenbothk <59426624+kittenbothk@users.noreply.github.com>
2022-01-26 15:21:31 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
-basic.forever(function () {
-	
-})
+/*
+by Anson from KittenBot HK
+
+PowerEdu designed for Micro:bit
+*/
+
+//% color='#ffb700' weight=10 icon="\uf2ce"
+//% group='["PowerEDU"]'
+
+namespace PowerEDU {
+let test=0
+
+//% blockId=test block="TEST"
+//% group="TEST" weight=100
+export function Test(): number{
+	return 100
+}
+}
